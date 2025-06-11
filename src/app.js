5618d87bc710b0b64ef97114742e177fd18ea17a
Matheus Lopes Ferreira <lopesferreira.matheus9@gmail.com>
2025-06-11 09:27:28 -0300

    Add license

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+// Chatbot da Câmara Municipal de São Paulo
+// (c) 2025 Optimus Data Technology - Uso permitido apenas para estudos
+// Contato: claudinei.goncalves@optimusdata.com.br | (11) 98185-5447
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
